8720b46d1e14deb0877080b53f2a5d216a8f6cef
Leandefilippis <leandefilippis@gmail.com>
2022-12-02 14:13:20 -0300

    env change

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { timeStamp } = require('console');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const { DB_URI } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(DB_URI, {
+  define: {
+    timestamps: false
+  },
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
