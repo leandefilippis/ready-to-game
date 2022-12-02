d551d2c74c7583e99cd2b01573281179a5675736
Leandefilippis <leandefilippis@gmail.com>
2022-12-02 15:53:07 -0300

    strict mode deleted

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,7 @@ import {store} from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <App></App>
   </Provider>,
   document.getElementById('root')
 );
